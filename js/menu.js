a11237d6f907ddca82b96cb70bd11fdd6e58d9ac
egrcc <zhaolujun1994@gmail.com>
2015-04-08 16:17:20 +0800

    sdf

--- js/menu.js
+++ js/menu.js
@@ -66,17 +66,41 @@ function initMenu(){
 	fileMenu.append(new global.gui.MenuItem({
 		label: 'Save',
 		click: function() {
+			global.$('#saveFileDialog').attr({"nwsaveas":"file.md"});
 			chooseFile("#saveFileDialog", function(filename){
 				var fs = require('fs');
 				var textEditor = global.$('#editor');
-				// fs.writeFile(filename, textEditor.val(), function(err) {
+				fs.writeFile(filename, textEditor.val(), function(err) {
+					if(err) {
+						console.log(err);
+					} else {
+						console.log("The file was saved!");
+					}
+				}); 
+				// fs.writeFile(filename, global.$('html').html(), function(err) {
 				// 	if(err) {
 				// 		console.log(err);
 				// 	} else {
 				// 		console.log("The file was saved!");
 				// 	}
 				// }); 
-				fs.writeFile(filename, global.$('html').html(), function(err) {
+			});
+		}
+	}));
+
+	var exportMenu = new global.gui.Menu();
+
+	exportMenu.append(new global.gui.MenuItem({
+		label: 'HTML',
+		click: function() {
+			// global.gui.App.quit();
+			console.log("html");
+			changeModeToViewer();
+			global.$('#saveFileDialog').attr({"nwsaveas":"file.html"});
+			chooseFile("#saveFileDialog", function(filename){
+				console.log("html print");
+				var fs = require('fs');
+				fs.writeFile(filename, global.window.document.documentElement.outerHTML, function(err) {
 					if(err) {
 						console.log(err);
 					} else {
@@ -84,8 +108,41 @@ function initMenu(){
 					}
 				}); 
 			});
+
 		}
 	}));
+
+	exportMenu.append(new global.gui.MenuItem({
+		label: 'PDF',
+		click: function() {
+			// global.gui.App.quit();
+			
+
+			console.log("pdf");
+			changeModeToViewer();
+			global.$('#saveFileDialog').attr({"nwsaveas":"file.pdf"});
+			chooseFile("#saveFileDialog", function(pdffilename){
+
+				var fs = require('fs');
+				var pdf = require('phantom-html2pdf');
+				var options = {
+								"html" : "./file.html",
+								"css" :	"./css/maxiang.css",
+								"js" : "./js/highlight/highlight.pack.js"
+							  };
+				console.log("pdf print");
+				pdf.convert(options, function(result) {
+				    result.toFile(pdffilename, function() {});
+				});
+			});
+		}
+	}));
+
+	fileMenu.append(new global.gui.MenuItem({
+		label: 'Export...',
+		submenu: exportMenu
+	}));
+
 	fileMenu.append(new global.gui.MenuItem({
 		label: 'Exit',
 		click: function() {
@@ -93,6 +150,72 @@ function initMenu(){
 		}
 	}));
 
+	var modeMenu = new global.gui.Menu();
+
+	modeMenu.append(new global.gui.MenuItem({
+		label: 'Editor:Viewer',
+		click: function() {
+			// global.gui.App.quit();
+			var Editor = global.$('.md_editor');
+			var Viewer = global.$('.md_result');
+			Editor.css({"float":"left",
+						"display":"inline",
+						"width":"49.8%",
+						"margin-left":"5px"});
+			Viewer.css({"float":"right",
+						"display":"inline",
+						"width":"49.5%",
+						"margin-left":"0px"});
+			global.$('.background').css({"background-color":"#ffffff"});
+		}
+	}));
+	modeMenu.append(new global.gui.MenuItem({
+		label: 'Viewer:Editor',
+		click: function() {
+			// global.gui.App.quit();
+			var Editor = global.$('.md_editor');
+			var Viewer = global.$('.md_result');
+			Editor.css({"float":"right",
+						"display":"inline",
+						"width":"49.8%",
+						"margin-left":"0px"});
+			Viewer.css({"float":"left",
+						"display":"inline",
+						"width":"50.2%",
+						"margin-left":"0px"});
+			global.$('.background').css({"background-color":"#272822"});
+		}
+	}));
+	modeMenu.append(new global.gui.MenuItem({
+		label: 'Editor',
+		click: function() {
+			// global.gui.App.quit();
+			var Editor = global.$('.md_editor');
+			var Viewer = global.$('.md_result');
+			Editor.css({"width":"99.5%",
+						"float":"right",
+						"display":"inline",
+						"margin-left":"5px"});
+			Viewer.css({"display":"none"});
+			global.$('.background').css({"background-color":"#ffffff"});
+		}
+	}));
+	modeMenu.append(new global.gui.MenuItem({
+		label: 'Viewer',
+		click: changeModeToViewer
+	}));
+
 	menubar.append(new global.gui.MenuItem({ label: 'File', submenu: fileMenu}));
+	menubar.append(new global.gui.MenuItem({ label: 'Mode', submenu: modeMenu}));
 	win.menu = menubar;
 };
+
+function changeModeToViewer() {
+	var Editor = global.$('.md_editor');
+	var Viewer = global.$('.md_result');
+	Viewer.css({"width":"99%",
+				"float":"right",
+				"display":"inline"});
+	Editor.css({"display":"none"});
+	global.$('.background').css({"background-color":"#ffffff"});
+}
